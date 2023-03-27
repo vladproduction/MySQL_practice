f023704db8eb3da877ffa3ace2bb41ce0fa49dda
VladBogda <vladbogdantsev@gmail.com>
2023-03-27 17:29:38 +0300

    WP working process

--- Person_SQL/src/main/java/Main.java
+++ Person_SQL/src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         Dao dao = new DaoImpl();
-        Person person1 = new Person(1,"Stan",1);
+        Person person1 = new Person(1,"David",3);
         //dao.create(person1);
         List<Person> persons = dao.readAll();
         for (int i = 0; i < persons.size(); i++) {
