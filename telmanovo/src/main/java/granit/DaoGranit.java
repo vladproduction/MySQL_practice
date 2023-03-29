86cc623a81809c9ba02350a426758627088aad2d
VladBogda <124387608+vladproduction@users.noreply.github.com>
2023-03-29 17:55:52 +0300

    Delete telmanovo directory

--- telmanovo/src/main/java/granit/DaoGranit.java
+++
@@ -1,8 +0,0 @@
-package granit;
-
-import java.util.List;
-
-public interface DaoGranit {
-    public void create(PlayerGranit pg);
-    public List<PlayerGranit> readAll();
-}
--- telmanovo/src/main/java/niva/DaoNiva.java
+++
@@ -1,8 +0,0 @@
-package niva;
-
-import java.util.List;
-
-public interface DaoNiva {
-    public void create(PlayerNiva pn);
-    public List<PlayerNiva> readAll();
-}
